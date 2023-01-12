c8039255c007d85ccbb865b55b0e6b8dad88b37e
Frozen_byte <frozen_byte@gmx.de>
2023-01-12 18:08:56 +0100

    feat: add basics for theme

--- library.js
+++ library.js
@@ -75,3 +75,35 @@ library.defineWidgets = async function (widgets) {
 
 	return widgets;
 };
+
+library.defineWidgetAreas = async function (areas) {
+		const locations = ['header', 'sidebar', 'footer'];
+		const templates = [
+				'categories.tpl', 'category.tpl', 'topic.tpl', 'users.tpl',
+				'unread.tpl', 'recent.tpl', 'popular.tpl', 'top.tpl', 'tags.tpl', 'tag.tpl',
+				'login.tpl', 'register.tpl',
+		];
+		function capitalizeFirst(str) {
+				return str.charAt(0).toUpperCase() + str.slice(1);
+		}
+
+		templates.forEach((template) => {
+				locations.forEach((location) => {
+						areas.push({
+								name: `${capitalizeFirst(template.split('.')[0])} ${capitalizeFirst(location)}`,
+								template: template,
+								location: location,
+						});
+				});
+		});
+
+		areas = areas.concat([
+				{
+						name: 'Account Header',
+						template: 'account/profile.tpl',
+						location: 'header',
+				},
+		]);
+		return areas;
+};
+
